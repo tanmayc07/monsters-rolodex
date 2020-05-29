a9ac60e168d3b8097ae074254da03a749030be4c
tanmayc07 <tanmayc32@gmail.com>
2020-05-29 11:37:16 +0530

    Updates

---
+++ precache-manifest.2b1d03a684ac728bc64c3b82a25bfcbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c39650bd6d11b794f0fd1ab13108435",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "da32bb4be7a234b882d2",
+    "url": "/monsters-rolodex/static/css/main.39e97399.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da32bb4be7a234b882d2",
+    "url": "/monsters-rolodex/static/js/main.41824a6d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
